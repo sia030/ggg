9e2af7cdea6eecda1e45d1ecbef5ad034ad187e4
Sia Yi <sia2772@naver.com>
2016-04-27 19:17:27 +0900

    omniauth-facebook 페이스북 로그인 셋팅 및 테스트

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -241,7 +241,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, '1142964459067609', '9fa1af54b82e3903a0936a1728d82763', scope: 'email', info_fields: 'email,name'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160427083302) do
+ActiveRecord::Schema.define(version: 20160427091202) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 20160427083302) do
     t.text     "profile"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
