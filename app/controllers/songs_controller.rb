15aef225e54bbffc7a2c99e9bcf0488c6bd9f850
Sia Yi <sia2772@naver.com>
2016-04-28 17:50:52 +0900

    Song new, create 액션과 관련 뷰 생성 => 새로운 Song 생성기능, Song validation 추가, authenticate_user! 메서드를 이용해서 접근 페이지 설정

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,6 @@
 class SongsController < ApplicationController
+	before_action :authenticate_user!, except: :index
+
 	def index
 		@songs = Song.all
 	end
@@ -8,12 +10,21 @@ class SongsController < ApplicationController
 	end
 
 	def new
+		@song = Song.new
 	end
 
 	def edit
 	end
 
 	def create
+		@song = Song.new(params.require(:song).permit(:title, :description))
+		@song.user = current_user
+		
+		if @song.save
+			redirect_to @song
+		else
+			render :new
+		end
 	end
 
 	def update
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,6 @@
 class Song < ActiveRecord::Base
-  belongs_to :user
-  has_many :comments, dependent: :destroy
+	validates_presence_of :title, :description, :user_id
+	
+	belongs_to :user
+	has_many :comments, dependent: :destroy
 end
