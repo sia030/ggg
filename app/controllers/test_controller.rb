a42f53c98388eaf14b1643ac665eab1cffab0f8b
Sia Yi <sia2772@naver.com>
2016-04-28 15:29:56 +0900

    테스트컨트롤러 및 라우트 삭제

--- app/controllers/test_controller.rb
+++
@@ -1,4 +0,0 @@
-class TestController < ApplicationController
-	def test
-	end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'test' => 'test#test'
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
