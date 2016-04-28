e4933e6c1acab28a1e222d9d8e3e1b6d1dce6b5c
Sia Yi <sia2772@naver.com>
2016-04-28 16:00:36 +0900

    Songs#index 액션과 목록 생성

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,24 @@
 class SongsController < ApplicationController
+	def index
+		@songs = Song.all
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def edit
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
 end
